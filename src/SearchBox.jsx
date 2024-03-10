98af487bf1e8c99e8cdc5aaaefed2829106bb7e4
harshrao26 <harshurao058@gmail.com>
2024-03-10 21:37:01 +0530

    Mob Res 600px

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -20,7 +20,7 @@ function SearchBox({ updateInfo }) {
         `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
       );
       let jsonRes = await res.json();
-      console.log(jsonRes)
+      console.log(jsonRes);
 
       const result = {
         temp: jsonRes.main.temp,
@@ -37,17 +37,16 @@ function SearchBox({ updateInfo }) {
         sunrise: jsonRes.sys.sunrise,
         sunset: jsonRes.sys.sunset,
       };
-      console.log(result)
+      console.log(result);
       return result;
-      
     } catch (err) {
-      setErrors(true); 
+      setErrors(true);
     }
   };
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    setErrors(false); 
+    setErrors(false);
     try {
       let weatherInfo = await getWeatherinfo();
       if (weatherInfo) {
@@ -62,9 +61,13 @@ function SearchBox({ updateInfo }) {
 
   return (
     <div className="search backdrop-sepia-0 bg-white/30">
-       <h3><img src="./cloudy-line.svg" alt="" />Weather App</h3>
+      <h3>
+        <img src="./cloudy-line.svg" alt="" />
+        Weather App
+      </h3>
       <form onSubmit={handleSubmit}>
-        <TextField
+        <input
+          type="text"
           id="city"
           label="City Name"
           variant="outlined"
@@ -72,9 +75,19 @@ function SearchBox({ updateInfo }) {
           value={city}
           onChange={handleChange}
         />
-        <Button type="submit" variant="contained">
-          Send
-        </Button>
+        {/* <TextField
+          
+        /> */}
+        
+        <button type="submit">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path>
+          </svg>
+        </button>
         {errors && <p>City not found</p>}
       </form>
     </div>
